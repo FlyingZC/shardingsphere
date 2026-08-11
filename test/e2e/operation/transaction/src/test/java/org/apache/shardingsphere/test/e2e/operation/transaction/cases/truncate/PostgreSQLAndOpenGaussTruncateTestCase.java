cd94668231de0128d7ab9bca866d035bbef6ff3c
ZhangCheng <chengzhang@apache.org>
2026-08-11 14:59:13 +0800

    Fix PostgreSQL truncate outcome assertions

--- test/e2e/operation/transaction/src/test/java/org/apache/shardingsphere/test/e2e/operation/transaction/cases/truncate/PostgreSQLAndOpenGaussTruncateTestCase.java
+++ test/e2e/operation/transaction/src/test/java/org/apache/shardingsphere/test/e2e/operation/transaction/cases/truncate/PostgreSQLAndOpenGaussTruncateTestCase.java
@@ -51,6 +51,9 @@ public final class PostgreSQLAndOpenGaussTruncateTestCase extends BaseTransactio
             connection.rollback();
             // Expected truncate operation can be rolled back in PostgreSQL & OpenGauss
             assertAccountRowCount(connection, 8);
+            try (Connection queryConnection = getDataSource().getConnection()) {
+                assertAccountRowCount(queryConnection, 8);
+            }
         }
     }
     
@@ -62,7 +65,9 @@ public final class PostgreSQLAndOpenGaussTruncateTestCase extends BaseTransactio
             executeWithLog(connection, "TRUNCATE account;");
             assertAccountRowCount(connection, 0);
             connection.commit();
-            assertAccountRowCount(connection, 0);
+            try (Connection queryConnection = getDataSource().getConnection()) {
+                assertAccountRowCount(queryConnection, 0);
+            }
         }
     }
     
